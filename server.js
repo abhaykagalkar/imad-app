1b8455cb0c07a25c99e0a30d3ffb94c12de9ab94
abhaykagalkar <abhaykagalkar@gmail.com>
2017-08-20 14:39:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/abhay', function (req, res) {
+  res.sendFile(path.join(__dirname, 'abhay', 'index.html'));
+});
+
+app.get('/sitemap', function (req, res) {
+  res.send('Sitemap is fetched and will be served');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
