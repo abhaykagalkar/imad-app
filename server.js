6bbc8a61aad71d039b57af46102202521abf5392
abhaykagalkar <abhaykagalkar@gmail.com>
2017-08-20 20:00:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title:'Abhay kagalkar Page',
+    date:'August 20,2017',
+    heading:'Article One',
+    content: `This is a Web Page of our site Made from imad.hasura-app.io,This is a Web Page of our site Made from imad.hasura-app.io,This is a Web Page of our site Made from imad.hasura-app.io,v,vvvvThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.ioThis is a Web Page of our site Made from imad.hasura-app.io`
+    
+};
+function createTemplate (data) {
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+    
+var htmlTemplate=`
+<!DOCTYPE html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+        <style>
+            .container{
+                max-width=800px;
+                margin=auto;
+                color=grey;
+                font-family=sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>&nbsp; &nbsp;
+                <a href="http://www.codesexplorer.com">CodesExplorer Home</a>
+            </div>
+            <hr/>
+            <br />
+            <h3>${heading}</h3>
+            <br/>
+            <div>
+                <h3>todays date ${date}   </h3>
+            </div>
+            <div>
+                <p>
+                   ${content}
+                </p>
+                <p>
+                   ${content}
+                </p>
+            </div>
+        </div>
+    </body>
+    
+</html>`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +70,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/codesexplorer',function (req,res) {
-    res.sendFile(path.join(__dirname,'ui','codesexplorer.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/abhay', function (req, res) {
